f84be70ace724a9e172bfd7adda3873792d40ddb
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-09-16 03:44:38 -0700

    basic file structures

---
+++ rubinaportfolio/src/constants/index.js
@@ -0,0 +1,176 @@
+// import {
+// } from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+// const experiences = [
+//     {
+//         title: "Web Designer",
+//         company_name: "Freelance",
+//         icon: backend,
+//         iconBg: "#383E56",
+//         date: "January 2020 - Present",
+//         points: [
+//             "Developing and maintaining web applications clients needs.",
+//             "Collaborating with clients to create high-quality products.",
+//             "Implementing responsive design and ensuring cross-browser compatibility.",
+//             "Participating in design reviews implimenting regular updates.",
+//         ],
+//     },
+//     {
+//         title: "Full-Stack Web Developer Certificaiton",
+//         company_name: "University of Washington",
+//         icon: meta,
+//         iconBg: "#E6DEDD",
+//         date: "June 2023",
+//         points: [
+//             "Develop a responsive,  user-friendly, intuitive, and secure full-stack website from with interactive features to cross-browser optimize traffic and user experience  using React, Node.js, and Express.",
+//             "Collaborate on a self-managed team of 4 developers to create high-quality products.",
+//             "Implemented automated testing using Jest frameworks for a REST website application",
+//             "Developed and deployed a fully-functioning web application using React, Node.js, and MySQL in 10 days.",
+//         ],
+//     },
+// ];
+
+
+// const projects = [
+//     {
+//         name: "One By Faith",
+//         description:
+//             "Web-based platform that allows users to search, book, and manage individual spiritual services online and in person as well as group events, providing a convenient and efficient solution to manage scheduling and take payment.",
+//         tags: [
+//             {
+//                 name: "Square",
+//                 color: "blue-text-gradient",
+//             },
+//             {
+//                 name: "AcuityScheduling",
+//                 color: "pink-text-gradient",
+//             },
+//         ],
+//         image: obfPoster,
+//         video: onebyfaith,
+//         source_code_link: "https://www.onebyfaith.co/",
+//     },
+//     {
+//         name: "DoodleDash",
+//         description:
+//             "A live, multiplayer web game that allows players to join a room and guess a randomized word based on a randomly selected player’s drawing on a live virtual white board before the time ends. Once players select to end the game the player with the most wins is announced!",
+//         tags: [
+//             {
+//                 name: "react",
+//                 color: "blue-text-gradient",
+//             },
+//             {
+//                 name: "bootstrap",
+//                 color: "green-text-gradient",
+//             },
+//             {
+//                 name: "mySQL",
+//                 color: "pink-text-gradient",
+//             },
+//         ],
+//         image: doodlePoster,
+//         video: DoodleDash,
+//         source_code_link: "https://github.com/17anguyen/notPictionary#tech",
+//     },
+//     {
+//         name: "June Portfolio",
+//         description:
+//             "This portfolio was built at the end of my class to display my understanding of developing a full stack web application. This was an unfinished design submitted as a final assignment with the intentiont to be redesigned!",
+//         tags: [
+//             {
+//                 name: "react",
+//                 color: "blue-text-gradient",
+//             },
+//             {
+//                 name: "bootstrap",
+//                 color: "green-text-gradient",
+//             },
+//             {
+//                 name: "mySQL",
+//                 color: "pink-text-gradient",
+//             },
+//         ],
+//         image: junePoster,
+//         video: juneportfolio,
+//         source_code_link: "https://github.com/17anguyen/class-grad-portfolio-jun2023",
+//     },
+//     {
+//         name: "ChinguTalk",
+//         description:
+//             "A social media application with with account creation and modificaiton. Users are logged into a database based on shared interests and interests can be searched to find friends! The deployed link is disabled due to heroku hosting costs.",
+//         tags: [
+//             {
+//                 name: "Node.js",
+//                 color: "blue-text-gradient",
+//             },
+//             {
+//                 name: "mySQL",
+//                 color: "green-text-gradient",
+//             },
+//             {
+//                 name: "Express Handlebars",
+//                 color: "pink-text-gradient",
+//             },
+//         ],
+//         image: chinguPoster,
+//         video: chingu,
+//         source_code_link: "https://github.com/17anguyen/chingu-talk",
+//     },
+//     {
+//         name: "Event Planner",
+//         description:
+//             "This project displays my progress with understanding data persistence and creating multiple usable pages. This application is connected to Google APIs for location data and Calandar access to add events in the application to your calendar and send invites to listed attendees!",
+//         tags: [
+//             {
+//                 name: "Google Location and Calendar API",
+//                 color: "blue-text-gradient",
+//             },
+//             {
+//                 name: "Materialize",
+//                 color: "green-text-gradient",
+//             },
+//             {
+//                 name: "Google Fonts",
+//                 color: "pink-text-gradient",
+//             },
+//         ],
+//         image: plannerPoster,
+//         video: eventplan,
+//         source_code_link: "https://github.com/17anguyen/event-planner",
+//     },
+//     {
+//         name: "First Class Portfolio",
+//         description:
+//             "The first portfolio I built for class to display my understanding of HTML and CSS!",
+//         tags: [
+//             {
+//                 name: "CSS",
+//                 color: "blue-text-gradient",
+//             },
+//             {
+//                 name: "HTML",
+//                 color: "pink-text-gradient",
+//             },
+//         ],
+//         image: folio1Poster,
+//         video: portfolio,
+//         source_code_link: "https://github.com/17anguyen/Portfolio-Project",
+//     },
+// ];
+
+// export { experiences, projects };
